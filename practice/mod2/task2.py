ae83947dc90e252ff3fc3550aeab235ca53662f0
AMaslyakov <a.v.masliakov@urfu.me>
2026-03-24 14:06:53 +0500

    Практика 2 Выполнена

--- practice/mod2/task2.py
+++ practice/mod2/task2.py
@@ -3,7 +3,7 @@ import re
 email_pattern = re.compile(r"[\d\w+_\-#]+@[\d\w]+\.\w{2,3}")
 
 if __name__ == "__main__":
-    print("Введите текст:")
+    print("Введите текст в котором есть email-ы:")
     TEXT = input()
     emails = email_pattern.findall(TEXT)
     print(*emails, sep="\n")
